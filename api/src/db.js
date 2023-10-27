6b04b31556969dd5853b22658f290f7f7c3f7adf
ImanolBraccciale <imanol11699@outlook.com>
2023-10-27 20:39:38 -0300

    cambio de db

--- api/src/db.js
+++ api/src/db.js
@@ -2,9 +2,9 @@ require('dotenv').config(); //sirve para leer el archivo .env con el usuario, pa
 const { Sequelize,Op  } = require('sequelize'); //importamos a sequelize para poder comunicar entre query y javascript
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const {  DB_URL} = process.env;
 const sequelize = new Sequelize( //instanciamos a sequelize con new, acá estamos conectando al server con la db, en este caso con postgres con las coordenandas de las claves y todo eso. 
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   `${DB_URL}`,
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
