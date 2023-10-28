4fb180edce9db3cf8838f7a332d06daed44bdcc5
ImanolBraccciale <imanol11699@outlook.com>
2023-10-27 21:49:30 -0300

    qweqweqwe

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors = require('cors'); // Import the CORS middleware
 
 require('./db.js');
 
@@ -10,22 +11,21 @@ const server = express();
 
 server.name = 'API';
 
-// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// server.use(bodyParser.json({ limit: '50mb' }));
-// server.use(cookieParser());
-// server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://pokemon-rho-ruddy.vercel.app'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+
+// Use CORS middleware to allow requests from a specific origin
+server.use(cors({
+  origin: 'https://pokemon-rho-ruddy.vercel.app',
+  credentials: true,
+}));
 
 server.use(routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
