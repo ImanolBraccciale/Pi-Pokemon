d5362dc18326942ac0176f657d6ccf61b6f5222c
ImanolBraccciale <imanol11699@outlook.com>
2024-02-02 10:33:30 -0300

    zzzzzzzzzzz

--- api/src/app.js
+++ api/src/app.js
@@ -17,12 +17,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 
 // Use CORS middleware to allow requests from a specific origin
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pokemon-rho-ruddy.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+ 
 server.use(routes);
 
 // Error catching endware.
