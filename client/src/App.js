f4e15034f14b6302d2a6094318c1d6dd15883f6b
ImanolBraccciale <imanol11699@outlook.com>
2024-02-02 10:18:02 -0300

    asdas

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Detail from "./views/Detail/detail.component";
 import Create from "./views/Create/create.component";
 import Landing from "./views/Landing/landing.component";
 import axios from 'axios';
-axios.defaults.baseURL="https://pokemon-oa45.onrender.com"
+axios.defaults.baseURL="https://pi-pokemon-zeta.vercel.app/"
 function App() {
   return (
     <div className="App">
